1ef810a321d22d7b0b6e7b12cf2c758341afa480
Rafael Batista <rafaelbatista9710@gmail.com>
2021-11-17 14:38:05 +0000

    Firebase config

--- SmartDoorBellApp/ViewController.swift
+++ SmartDoorBellApp/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
+    var ref: DatabaseReference!
+
+ 
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = Database.database().reference()
         // Do any additional setup after loading the view.
     }
 
-
+   
 }
 
